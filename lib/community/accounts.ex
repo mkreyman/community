46c89fa6039d6184601c8f6fb2dce993806fbb8c
mkreyman <mark@kreyman.com>
2019-02-04 16:48:27 -0700

    Added more actions to user controller

--- lib/community/accounts.ex
+++ lib/community/accounts.ex
@@ -4,22 +4,31 @@ defmodule Community.Accounts do
   """
 
   alias Community.Accounts.User
-
-  def list_users do
-    [
-      %User{id: "1", name: "JosÃ©", username: "josevalim"},
-      %User{id: "2", name: "Bruce", username: "redrapids"},
-      %User{id: "3", name: "Chris", username: "chrismccord"}
-    ]
-  end
+  alias Community.Repo
 
   def get_user(id) do
-    Enum.find(list_users(), fn map -> map.id == id end)
+    Repo.get(User, id)
+  end
+
+  def get_user!(id) do
+    Repo.get!(User, id)
   end
 
   def get_user_by(params) do
-    Enum.find(list_users(), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end)
+    Repo.get_by(User, params)
+  end
+
+  def list_users do
+    Repo.all(User)
+  end
+
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
+  end
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
   end
 end
--- lib/community_web/router.ex
+++ lib/community_web/router.ex
@@ -20,9 +20,8 @@ defmodule CommunityWeb.Router do
   scope "/", CommunityWeb do
     pipe_through :browser
 
-    get "/users", UserController, :index
-    get "/users/:id", UserController, :show
     get "/", PageController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
   end
 
   # Other scopes may use custom stacks.
