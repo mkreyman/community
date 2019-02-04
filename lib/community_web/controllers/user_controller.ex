770627278749a97b22c1ed5827289cbf6ce676da
mkreyman <mark@kreyman.com>
2019-02-04 14:05:00 -0700

    Created user model and migration

--- lib/community_web/controllers/user_controller.ex
+++ lib/community_web/controllers/user_controller.ex
@@ -7,4 +7,9 @@ defmodule CommunityWeb.UserController do
     users = Accounts.list_users()
     render(conn, "index.html", users: users)
   end
+
+  def show(conn, %{"id" => id}) do
+    user = Accounts.get_user(id)
+    render(conn, "show.html", user: user)
+  end
 end
